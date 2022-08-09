53d5336de687888a5e627843c2adfec4334ac585
Vinicius Emanoel Mendes <mendes.vinicius7575@gmail.com>
2022-08-09 07:53:59 -0300

    Primeira checagem de erros

--- js/javas.js
+++ js/javas.js
@@ -1,3 +1,10 @@
+//recebdendo os querys
+let form = document.querySelector('#register')
+let submit = document.querySelector('#btn-submit')
+
+
+
+
 //iniciando a classe validação
 class Validator{
     constructor(){
@@ -14,36 +21,63 @@ class Validator{
 
         //pegar os inputs-no caso isso é um HTML Collection
         let inputs = form.getElementsByTagName('input')
-
-        console.log(`ola`, inputs[0])
         
 
         //Transformar um HTML Collection em um Array
         let inputsArray = [...inputs]
-
-        console.log(`ola2`, inputsArray[0])
 
         console.log(`ae`,inputsArray[0].textContent)
 
         //loop de inputs e validação para cada um deles
         inputsArray.forEach(function(input){
             for(let i =0; this.validations.length > i; i++){
+                //verifica se a validação atual existe no input
                 if(input.getAttribute(this.validations[i]) != null){
-                    console.log('achou validação')
+                    //limpa a string para virar um metodo
+                    let method = this.validations[i].replace('data-','').replace('-','');
+                    //valor input
+                    let valor = input.getAttribute(this.validations[i]);
+                    //chama o metodo
+                    this[method](input, valor);
                 }
             }
         }, this);
+
+        
+    }
+    //verifica se tem o num minimo de caracteres no campo
+    minlength(input, minValor){
+
+        let inputLength = input.value.length;
+
+        let errorMessage = `O Campo precisa ter pelo menos ${minValor}`;
+
+        if(inputLength < minValor){
+            this.mostrarMensagem(input, errorMessage)
+        }
+
+    }
+
+    //metodo para imprimir a mensagemd e erro
+    mostrarMensagem(input, msg){
+        let template = document.querySelector('.class-error').cloneNode(true);
+
+        template.textContent = msg;
+
+        let inputParent = input.parentNode;
+
+        template.classList.remove('template');
+
+        inputParent.appendChild(template)
+
+
     }
 }
-//recebdendo os querys
-let form = document.querySelector('#register')
-let submit = document.querySelector('#btn-submit')
+
+
+
+
 let validator = new Validator();
-
-
-
-
-
 
 
 
